31ae2cce199923e78a1feddac84bc71c7d9694cc
Jack Ramsey <jram7533@gmail.com>
2020-03-22 15:03:11 -0400

    Add files via upload

---
+++ dnd.js
@@ -0,0 +1,14 @@
+/*
+    add this script to client's landing page as needed
+*/
+
+var daysDeadline = 30;
+var body = document.getElementsByTagName('body')[0];
+
+body.style.opacity = 1;
+
+var interval = setInterval(function(){ 
+    daysDeadline--;
+    body.style.opacity -= .03;  // reduce page's opacity
+    if (daysDeadline <= 0){ clearInterval(interval); }
+}, 86400000);   // one day
